16034efa8bbf3afcbddf228aa59e62afbba9a326
Des Shearer <desmond.shearer@hscni.net>
2021-09-21 12:41:23 +0100

    updated server from master

--- server.R
+++ server.R
@@ -1,127 +1,136 @@
-####### SETUP  ####### 
-
-# Libraries
-library(shinydashboard)
-library(tidyverse)
-library(plyr)
-library(lubridate)
-library(DT)
-library(ggplot2)
-library(plotly)
-
-#detach("package:plyr", unload = TRUE)
-
-
-# Source scripts
-source("./azure_functions.R")
-
-# Load data
-locations <- getTable("Locations")
-collectclosecontacts <- getTable("CollectContactsCalls")
-cases <- getTable("cases")
-
-# Function
 function(input, output, session) {
   
-  # Build Data Frames
-  
-  ## Education
-  education <- locations %>%
-    filter(TypeOfPlace == "School or college") %>%
-    left_join(collectclosecontacts, by = c("CollectCallId" = "Id")) %>%
-    left_join(cases, by = "CaseNumber")
-  
-  
-  ## Education Group Lists
-  
-  education.group.list <- education %>%
-    group_by(InstitutionName.x, City.x, InstitutionType, InstitutionReferenceNumber) %>% 
-    tally()
-  
-  education.group.list.28days <- education %>%
-    filter(CreatedOn.x >= Sys.Date()-28) %>% 
-    group_by(InstitutionName.x, City.x, InstitutionType, InstitutionReferenceNumber) %>% 
-    tally() 
-  
-  education.group.list.7days <- education %>%
-    filter(CreatedOn.x >= Sys.Date()-7) %>% 
-    group_by(InstitutionName.x, City.x, InstitutionType, InstitutionReferenceNumber) %>% 
-    tally() 
-  
-  education.group.list.4to6days <- education %>%
-    filter(CreatedOn.x >= Sys.Date()-6 & CreatedOn.x <= Sys.Date()-4) %>% 
-    group_by(InstitutionName.x, City.x, InstitutionType, InstitutionReferenceNumber) %>% 
-    tally() 
-  
-  education.group.list.3days <- education %>%
-    filter(CreatedOn.x >= Sys.Date()-3) %>% 
-    group_by(InstitutionName.x, City.x, InstitutionType, InstitutionReferenceNumber) %>% 
-    tally() 
-  
-  education.group.table <- education.group.list.28days %>% 
-    left_join(education.group.list.7days[ , -c(2:4)], by = "InstitutionName.x") %>% 
-    left_join(education.group.list.4to6days[ , -c(2:4)], by = "InstitutionName.x") %>%
-    left_join(education.group.list.3days[ , -c(2:4)], by = "InstitutionName.x") %>% 
-    drop_na() %>%
-    distinct(InstitutionReferenceNumber, .keep_all = TRUE) %>% 
-    mutate(CaseTrend = case_when(
-      n.y.y > n.x.x ~ c('Up'),
-      n.y.y < n.x.x ~ c('Down'),
-      n.y.y == n.x.x ~ c('Stable')))
-    
-  education.group.table <- education.group.table[order(-education.group.table$n.x), ]
-  colnames(education.group.table) <- c("Institution Name", "City", "Institution Type", "Reference Number", "Cases Last 28 Days", "Cases Last 7 Days", "Cases Last 4 to 6 Days", "Cases Last 3 Days", "Trend")
-  
-  ## Downloadable csv of selected dataset 
-  output$DownloadHomeReport <- downloadHandler(
-    filename = function() {
-      paste("Prev28DayEducationReport-", Sys.Date(), ".csv", sep="")
-    },
-    content = function(file) {
-      write.csv(education.group.table, file, row.names = FALSE)
-    }
-  )
-  
+  ####### HOME  ####### 
   # InfoBoxes
   
   ## Total Cases
   output$total_cases <- renderInfoBox({
     infoBox(
       "Total Reported Cases from Education Insitutions", 
-      paste0(formatC(nrow(filter(education)), format="d", big.mark=",")), 
-      icon = icon("chart-bar"), 
+      paste0(formatC(nrow(schools_cases), format="d", big.mark=",")), 
+      icon = icon("graduation-cap"), 
       color ="blue")
-  })
-  ## Total Cases this Month
-  output$cases_this_month <- renderInfoBox({
-    infoBox(
-      "Reported Cases This Month from Education Insitutions", 
-      paste0(formatC(nrow(education %>% 
-                         filter(CreatedOn.x >= Sys.Date()-28)), format="d", big.mark=",")), 
-      icon = icon("virus"), 
-      color = "light-blue")
   })
   
   ## Total Schools
   output$total_groups <- renderInfoBox({
     infoBox(
       "Total Reported Affected Education Insitutions", 
-      paste0(formatC(nrow(filter(education.group.list)), format="d", big.mark=",")), 
+      paste0(formatC(nrow(schools_stats_overall %>%
+                            distinct(DENINumber, .keep_all = TRUE) %>% 
+                            drop_na(InstitutionName)),
+                     format="d", big.mark=",")), 
       icon = icon("school"), 
       color ="blue")
   })
-  ## Total Schools this Month
-  output$groups_this_month <- renderInfoBox({
+  
+  ## Total Cases Last Week
+  output$cases_last_week <- renderInfoBox({
     infoBox(
-      "Education Insitutions Affected This Month", 
-      paste0(formatC(nrow(education.group.list.28days), format="d", big.mark=",")), 
+      "Reported Cases Last Week from Education Insitutions", 
+      paste0(formatC(nrow(schools_cases %>% 
+                            filter(DateOfSample.x >= Sys.Date()-14 & DateOfSample.x <= Sys.Date()-8)), format="d", big.mark=",")), 
       icon = icon("graduation-cap"), 
       color = "light-blue")
   })
-
-  # Frequency Table
-  output$education_cases_table = DT::renderDataTable({
-    DT::datatable(education.group.table, options = list(pageLength = 25))
+  
+  ## Total Schools Last Week
+  output$groups_last_week <- renderInfoBox({
+    infoBox(
+      "Education Insitutions Affected Last Week", 
+      paste0(formatC(nrow(schools_cases_w_wgs %>%
+                            filter(DateOfSample.x >= Sys.Date()-14 & DateOfSample.x <= Sys.Date()-8) %>% 
+                            group_by(InstitutionReferenceNumber) %>% 
+                            tally()),
+                     format="d", big.mark=",")), 
+      icon = icon("school"), 
+      color = "light-blue")
   })
   
+  ## Total Cases This Week
+  output$cases_this_week <- renderInfoBox({
+    infoBox(
+      "Reported Cases This Week from Education Insitutions", 
+      paste0(formatC(nrow(schools_cases %>% 
+                            filter(DateOfSample.x >= Sys.Date()-7)),
+                     format="d", big.mark=",")), 
+      icon = icon("graduation-cap"), 
+      color = "navy")
+  })
+  
+  ## Total Schools This Week
+  output$groups_this_week <- renderInfoBox({
+    infoBox(
+      "Education Insitutions Affected This Week", 
+      paste0(formatC(nrow(schools_cases_w_wgs %>%
+                            filter(DateOfSample.x >= Sys.Date()-7) %>% 
+                            group_by(InstitutionReferenceNumber) %>% 
+                            tally()),
+                     format="d", big.mark=",")), 
+      icon = icon("school"), 
+      color = "navy")
+  })
+  
+  
+  # Frequency Table
+  
+  ## Build Table 
+  
+  home.page.table <- schools_stats_overall %>% 
+    select(InstitutionName, Town, InstitutionType, DENINumber, CasesPrev28Days, CasesWithinLast6Days, CasesWithinLast4to6Days,
+           CasesWithinLast3Days, TotalPupils, AttackRate, CaseTrend)
+  
+  home.page.table <- home.page.table[order(-home.page.table$CasesPrev28Days), ]
+  home.page.table$Town <- tolower(home.page.table$Town)
+  home.page.table$Town <- toTitleCase(home.page.table$Town)
+  colnames(home.page.table) <- c("Institution Name", "Town", "Institution Type", "DENI Number", "28 Days Cases", "7 Days Cases",
+                                 "4 to 6 Days Cases", "3 Days Cases", "Total Pupils",
+                                 "28 Day Attack Rate (%)", "Trend")
+  
+  ## Downloadable csv of Table 
+  output$DownloadHomeReport <- downloadHandler(
+    filename = function() {
+      paste("Prev28DayEducationReport-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(home.page.table, file, row.names = FALSE)
+    }
+  )
+  
+  ## Render Home Page Table
+  output$education_cases_table = DT::renderDataTable({
+    DT::datatable(home.page.table,
+                  options = list(pageLength = 25))
+  })
+  
+  ####### SCHOOL YEAR GRAPH ####### 
+  
+  # Build School Data Frame
+  output$school_year_table <- renderPlotly({
+    
+    school.year.table <- schools_cases %>% 
+      filter(InstitutionReferenceNumber == input$input_school_id) %>% 
+      select(CaseNumber, Gender.x, SchoolYear) %>% 
+      mutate(SchoolYear = as.factor(SchoolYear)) 
+    
+    
+    school.year.table.plot <- ggplot(data = school.year.table, aes(x = SchoolYear, fill = Gender.x)) + 
+      geom_bar(data = subset(school.year.table, Gender.x == "Female")) + 
+      geom_bar(data = subset(school.year.table, Gender.x == "Male"), aes(y =..count..*(-1))) + 
+      coord_flip() +
+      labs(title = paste("Case Frequencies for", input$input_school_id), 
+           x = "School Group", 
+           y = "Frequency",
+           fill = "Gender") +
+      theme_bw()
+    
+    ggplotly(school.year.table.plot)
+    
+  }) 
+  
 }
+
+
+
+
+
